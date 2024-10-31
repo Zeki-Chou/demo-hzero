a0632976052855f5274ca9ae90891a130610d350
fatihk25 <fatihkhoiri25@gmail.com>
2024-10-31 16:48:26 +0700

    [ADD] Add new Task and User functionality from DDD

---
+++ src/main/java/com/hand/demo/infra/mapper/UserMapper.java
@@ -0,0 +1,23 @@
+package com.hand.demo.infra.mapper;
+
+import io.choerodon.mybatis.common.BaseMapper;
+import com.hand.demo.domain.entity.User;
+
+import java.util.List;
+
+/**
+ * User Table(User)应用服务
+ *
+ * @author
+ * @since 2024-10-31 16:43:05
+ */
+public interface UserMapper extends BaseMapper<User> {
+    /**
+     * 基础查询
+     *
+     * @param user 查询条件
+     * @return 返回值
+     */
+    List<User> selectList(User user);
+}
+
