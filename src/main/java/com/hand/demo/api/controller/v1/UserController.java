8acb4dabd079e7e62035bdfd89abef550be7d667
zeki <1359637538@qq.com>
2024-10-31 15:37:00 +0700

    [ADD] Generate code about User for the first time

---
+++ src/main/java/com/hand/demo/api/controller/v1/UserController.java
@@ -0,0 +1,77 @@
+package com.hand.demo.api.controller.v1;
+
+import io.choerodon.core.domain.Page;
+import io.choerodon.core.iam.ResourceLevel;
+import io.choerodon.mybatis.pagehelper.annotation.SortDefault;
+import io.choerodon.mybatis.pagehelper.domain.PageRequest;
+import io.choerodon.mybatis.pagehelper.domain.Sort;
+import io.choerodon.swagger.annotation.Permission;
+import io.swagger.annotations.ApiOperation;
+import org.hzero.core.base.BaseController;
+import org.hzero.core.util.Results;
+import org.hzero.mybatis.helper.SecurityTokenHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.hand.demo.app.service.UserService;
+import com.hand.demo.domain.entity.User;
+import com.hand.demo.domain.repository.UserRepository;
+import springfox.documentation.annotations.ApiIgnore;
+
+import java.util.List;
+
+/**
+ * User Table(User)表控制层
+ *
+ * @author Zeki
+ * @since 2024-10-31 15:34:15
+ */
+
+@RestController("userController.v1")
+@RequestMapping("/v1/{organizationId}/users")
+public class UserController extends BaseController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserService userService;
+
+    @ApiOperation(value = "User Table列表")
+    @Permission(level = ResourceLevel.ORGANIZATION)
+    @GetMapping
+    public ResponseEntity<Page<User>> list(User user, @PathVariable Long organizationId, @ApiIgnore @SortDefault(value = User.FIELD_ID,
+            direction = Sort.Direction.DESC) PageRequest pageRequest) {
+        Page<User> list = userService.selectList(pageRequest, user);
+        return Results.success(list);
+    }
+
+    @ApiOperation(value = "User Table明细")
+    @Permission(level = ResourceLevel.ORGANIZATION)
+    @GetMapping("/{id}/detail")
+    public ResponseEntity<User> detail(@PathVariable Long id) {
+        User user = userRepository.selectByPrimary(id);
+        return Results.success(user);
+    }
+
+    @ApiOperation(value = "创建或更新User Table")
+    @Permission(level = ResourceLevel.ORGANIZATION)
+    @PostMapping
+    public ResponseEntity<List<User>> save(@PathVariable Long organizationId, @RequestBody List<User> users) {
+        validObject(users);
+        SecurityTokenHelper.validTokenIgnoreInsert(users);
+        userService.saveData(users);
+        return Results.success(users);
+    }
+
+    @ApiOperation(value = "删除User Table")
+    @Permission(level = ResourceLevel.ORGANIZATION)
+    @DeleteMapping
+    public ResponseEntity<?> remove(@RequestBody List<User> users) {
+        SecurityTokenHelper.validToken(users);
+        userRepository.batchDeleteByPrimaryKey(users);
+        return Results.success();
+    }
+
+}
+
